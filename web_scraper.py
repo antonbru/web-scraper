297a7bbdc769079b2dccae312bb9a97c936dd6ea
Anton Belitskiy <anton.belitskiy@gmail.com>
2024-11-23 11:20:36 +0300

    Запрос адреса сайта

--- web_scraper.py
+++ web_scraper.py
@@ -1,27 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-   # URL страницы, которую вы хотите открыть
-url = 'https://orshariver.club'  # Замените на нужный вам URL
+def save_webpage_text_to_file():
+    # Запрашиваем у пользователя URL адрес сайта
+    url = input("Введите URL адрес сайта: ")
 
-try:
-       # Отправляем запрос на страницу
-       response = requests.get(url)
-       response.raise_for_status()  # Проверка на успешный ответ
+    try:
+        # Отправляем запрос на страницу
+        response = requests.get(url)
+        response.raise_for_status()  # Проверка на успешный ответ
 
-       # Создаем объект BeautifulSoup для разбора страницы
-       soup = BeautifulSoup(response.text, 'html.parser')
+        # Создаем объект BeautifulSoup для разбора страницы
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-       # Извлекаем текст со страницы
-       page_text = soup.get_text()
+        # Извлекаем текст со страницы
+        page_text = soup.get_text()
 
-       # Указываем имя файла, в который сохраним текст
-       filename = 'webpage_text.txt'
+        # Указываем имя файла, в который сохраним текстщкы
+        filename = 'webpage_text.txt'
 
-       # Сохраняем текст в файл
-       with open(filename, 'w', encoding='utf-8') as file:
-           file.write(page_text)
+        # Сохраняем текст в файл
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(page_text)
 
-       print(f'Текст страницы успешно сохранён в файле {filename}')
-except requests.exceptions.RequestException as e:
-       print(f"Произошла ошибка при попытке получить доступ к URL: {e}")
+        print(f'Текст страницы успешно сохранён в файле {filename}')
+    except requests.exceptions.RequestException as e:
+        print(f"Произошла ошибка при попытке получить доступ к URL: {e}")
+
+# Вызов функции для сохранения содержимого сайта
+save_webpage_text_to_file()
